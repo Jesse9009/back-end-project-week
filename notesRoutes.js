01a43e5606cbf2cd2394a2cd8cef57e91bafdb26
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-01-18 23:07:57 -0600

    changes error message

--- notesRoutes.js
+++ notesRoutes.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
         : res.status(404).json({ Err: 'No notes found' });
     })
     .catch(err => {
-      res.status(500).json(err);
+      res.status(500).json({ message: 'Error getting notes' });
     });
 });
 
